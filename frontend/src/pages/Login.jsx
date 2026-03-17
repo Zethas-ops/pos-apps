80abb5d85d879c5a65e59312eb8853b32b4f33d0
Zethas-ops <fauzizuhdi.h@gmail.com>
2026-03-17 11:15:37 +0700

    fix: set login token

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -39,6 +39,8 @@ function Login() {
       console.log("LOGIN SUCCESS");
 
       localStorage.setItem("user", JSON.stringify(data));
+      localStorage.setItem("token", "login-success");
+
       navigate("/");
 
     } catch (err) {
